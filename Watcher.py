f5def1f2b53d5934c141aeff6636d77bd8476067
TTornado3001 <rojakhan25@gmail.com>
2026-09-20 11:09:37 +0200

    Added a Watcher class with initial methods for observation. Added an initial Handler class

--- Watcher.py
+++ Watcher.py
@@ -1,17 +1,35 @@
 import watchdog.observers as Observer
-import watchdog.events as FileSystesmEventHandler
+import watchdog.events as FileSystemEventHandler
 import os
 import time
 
-class Watcher:
 
-    def __init__(self, fileToRun):
-        self.observer = Observer
-        self.fileToRun = fileToRun
+class Watcher:
+    DIRECTORY_TO_WATCH = r"C:\Users\Admin\Downloads"
+
+    def __init__(self, script_to_run):
+        self.observer = Observer()
+        self.script_to_run = script_to_run
 
     def run(self):
-        
+        event_Handler = Handler(self.script_to_run)
+        self.observer.schedule(event_Handler, ".", recursive=True)
+        self.observer.start()
+
+        try:
+            while True:
+                time.sleep(5)
+        finally:
+            self.observer.stop()
+            self.observer.join()
 
 
+class Handler(FileSystemEventHandler):
 
-class Handler:
+    def __init__(self, script_to_run):
+        self.script_to_run = script_to_run
+
+
+if __name__ == '__main__':
+    w = Watcher(r"C:\Users\Admin\PycharmProjects\PDFManager\Classifier")
+    w.run()
